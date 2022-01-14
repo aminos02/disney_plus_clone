4b5c738e0d8d84c019edd26f2b9f6bcd73eabe42
aminos02 <azzouzamine02@gmail.com>
2022-01-14 12:18:06 +0100

    ImgSlider

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -44,7 +44,8 @@ const Nav=styled.nav`
     display:flex;
     align-items:center;
     padding:0 36px;
-`
+    overflow-x:hidden;
+    `
 
 const Logo=styled.img`
     width:80px;
---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+import ImgSlider from './ImgSlider'
+const Home = () => {
+    return (
+        <Container>
+            <ImgSlider/>
+        </Container>
+    )
+}
+
+export default Home
+const Container=styled.main`
+min-height:calc(100vh - 70px);
+padding:0 calc(3.5vw + 5px);
+position:relative;
+overflow-x:hidden;
+&:before{
+    background: url("/images/home-background.png") center center / cover
+    no-repeat fixed;
+    content:"";
+    position:absolute;
+    right:0;
+    top:0;
+    left:0;
+    bottom:0;
+    z-index:-1;
+    }
+`
